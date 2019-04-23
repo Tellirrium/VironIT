93e24aef94b5438a13eba3c912b2bbe01fdd7e58
Tellirrium <artem.suvazu@gmail.com>
2019-04-23 13:19:18 +0300

    solution

---
+++ homework1.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function EventEmitor() {
+	this.eventTable = {};
+}
+
+EventEmitor.prototype.on = function(eventName, handler) {
+	if ( !this.eventTable[eventName] ) {
+		this.eventTable[eventName] = [];
+	}
+
+	this.eventTable[eventName].push(handler);
+
+	return () => this.eventTable[eventName].splice(this.eventTable[eventName].indexOf(handler), 1);
+}
+
+EventEmitor.prototype.emit = function(eventName, ...params) {
+	this.eventTable[eventName].forEach( elem =>  elem.apply(null, params) );
+}
+
+const a = new EventEmitor();
+a.on('one', (e = 1,m =2, r = 3) => console.log(e,m,r));
+const f = a.on('one', () => console.log('world'));
+a.on('two', () => console.log('hey'));
+a.emit('one');
+f();
+a.emit('one');
